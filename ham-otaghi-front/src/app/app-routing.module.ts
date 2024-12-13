d62bc50dd7b20296a5d5581b9f333a9914e6c571
Dorsa Maddah <dorsamaddah1380@gmail.com>
2024-12-13 17:15:35 +0330

    Add files via upload

---
+++ ham-otaghi-front/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./components/logging.module').then(m => m.LoggingModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled',
+    preloadingStrategy: PreloadAllModules,
+    scrollPositionRestoration: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
