8a6cdf0faa4bebc6d7580869468a45689dd6780f
Azad Kavian <azadkavian@gmail.com>
2024-12-18 09:04:51 +0330

    added shared app layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppFooterComponent, AppNavbarComponent } from './components';
+import { AppLayoutComponent } from './layouts';
 
 @NgModule({
-  declarations: [AppFooterComponent, AppNavbarComponent],
+  declarations: [AppFooterComponent, AppNavbarComponent, AppLayoutComponent],
   imports: [CommonModule, RouterModule],
   exports: [AppFooterComponent, AppNavbarComponent],
   providers: [],
