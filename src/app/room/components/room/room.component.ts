e271c0fda344b6aca1e3e152d58e9ae2bf2cf7b6
Azad Kavian <azadkavian@gmail.com>
2025-01-25 16:52:01 +0330

    optimized the suggested rooms page ui and texts

--- src/app/room/components/room/room.component.ts
+++ src/app/room/components/room/room.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { translateIndexToPersianText } from 'src/app/shared/util';
 
 @Component({
   selector: 'room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css'],
 })
-export class RoomComponent {}
+export class RoomComponent {
+  @Input() index: number = 0;
+
+  get persianIndexText(): string {
+    return translateIndexToPersianText(this.index);
+  }
+}
--- src/app/room/components/roommate/roommate.component.ts
+++ src/app/room/components/roommate/roommate.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { translateIndexToPersianText } from 'src/app/shared/util';
 
 @Component({
   selector: 'roommate',
   templateUrl: './roommate.component.html',
   styleUrls: ['./roommate.component.css'],
 })
-export class RoommateComponent {}
+export class RoommateComponent {
+  @Input() index: number = 0;
+
+  get persianIndexText(): string {
+    return translateIndexToPersianText(this.index);
+  }
+}
