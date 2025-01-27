7c169d7a42e5e05a89b080531fa9c129c2b6c218
Azad Kavian <azadkavian@gmail.com>
2025-01-27 11:44:49 +0330

    made the survey form choices persistent

--- src/app/survey/components/survey-item/survey-item.component.ts
+++ src/app/survey/components/survey-item/survey-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Survey } from '../../models';
 import { SurveyService } from '../../api';
 
@@ -7,12 +7,21 @@ import { SurveyService } from '../../api';
   templateUrl: './survey-item.component.html',
   styleUrls: ['./survey-item.component.css'],
 })
-export class SurveyItemComponent {
+export class SurveyItemComponent implements OnInit {
   @Input() data: Survey.Question | null = null;
 
   public selectedChoiceId = -1;
 
   constructor(private readonly _surveyService: SurveyService) {}
+
+  ngOnInit(): void {
+    if (this.data) {
+      this.selectedChoiceId =
+        this._surveyService.submissionDto$.getValue()[
+          this.data.id.toString()
+        ] ?? -1;
+    }
+  }
 
   selectItem(id: number) {
     this.selectedChoiceId = this.selectedChoiceId === id ? -1 : id;
